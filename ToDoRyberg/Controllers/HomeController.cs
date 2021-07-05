ec243f7cadb9dcae3f6085019111667a1acc61c1
alexr151 <61523445+alexr151@users.noreply.github.com>
2021-07-04 19:47:45 -0500

    Add project files.

---
+++ ToDoRyberg/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoRyberg.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoRyberg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ToDoContext context;
+        public HomeController(ToDoContext ctx) => context = ctx;
+
+
+        public IActionResult Index(string id)
+        {
+            var filters = new Filters(id);
+            ViewBag.Filters = filters;
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.Statuses.ToList();
+            ViewBag.DueFilters = Filters.DueFilterValues;
+
+            IQueryable<ToDo> query = context.ToDos
+                .Include(t => t.Category)
+                .Include(t => t.Status);
+
+            if (filters.HasCategory)
+            {
+                query = query.Where(c => c.CategoryId == filters.CategoryId);
+            }
+
+            if (filters.HasSatus)
+            {
+                query = query.Where(c => c.StatusId == filters.StatusId);
+            }
+
+            if (filters.HasDue) { 
+                var today = DateTime.Today;
+                if (filters.IsPast)
+                {
+                    query = query.Where(t => t.DueDate < today);
+                }
+
+                if (filters.IsFuture)
+                {
+                    query = query.Where(t => t.DueDate > today);
+                }
+
+                if (filters.IsToday)
+                {
+                    query = query.Where(t => t.DueDate == today);
+                }
+            }
+
+            var tasks = query.OrderBy(t => t.DueDate).ToList();
+            return View(tasks);
+        }
+        
+        [HttpGet]
+        public IActionResult Add(string description, DateTime dueDate)
+        {
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.Statuses.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ToDo task)
+        {
+            if (ModelState.IsValid)
+            {
+                context.ToDos.Add(task);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            else
+            {
+                ViewBag.Categories = context.Categories.ToList();
+                ViewBag.Statuses = context.Statuses.ToList();
+                return View(task);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            string id = string.Join('-', filter);
+            return RedirectToAction("Index", new { ID = id });
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] string id, ToDo selected)
+        {
+            if (selected.StatusId == null)
+            {
+                context.ToDos.Remove(selected);
+            }
+            else
+            {
+                string newStatusId = selected.StatusId;
+                selected = context.ToDos.Find(selected.Id);
+                selected.StatusId = newStatusId;
+                context.ToDos.Update(selected);
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", new { ID = id });
+        }
+        
+    }
+
+}
---
+++ ToDoRyberg/Models/ToDoContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoRyberg.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = "work", Name = "Work"},
+                new Category { CategoryId = "home", Name = "Home" },
+                new Category { CategoryId = "ex", Name = "Exercise" },
+                new Category { CategoryId = "shop", Name = "Shopping" },
+                new Category { CategoryId = "call", Name = "Contact" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "open", Name = "Open"},
+                new Status { StatusId = "closed", Name = "Completed"}
+                //new Status { StatusId = "inprogress", Name = "In Progress"},
+                //new Status { StatusId = "qa", Name = "Quality Assurance"}
+                );
+        }
+
+
+    }
+}
