c9378c960b27ac02ca43ee1705e8786baf9f63a3
alexr151 <61523445+alexr151@users.noreply.github.com>
2021-07-23 17:51:07 -0500

    Added tag helpers

---
+++ ToDoRyberg/Models/ButtonTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoRyberg.Models
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.AppendCssClass("btn btn-primary");
+        }
+    }
+}
---
+++ ToDoRyberg/Models/SubmitButtonTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoRyberg.Models
+{
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.BuildTag("button", "btn btn-primary");
+            output.Attributes.SetAttribute("type", "submit");
+        }
+    }
+}
